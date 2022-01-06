526990debac44c88a6fa3ccc9550806f075ac2cb
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-06 13:41:13 -0600

    Update Dev10 M07 - Web Scraping with BeautifulSoup.py

--- Dev10 M07 - Web Scraping with BeautifulSoup.py
+++ Dev10 M07 - Web Scraping with BeautifulSoup.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 def getHTML(url):
     response = requests.get(url)
@@ -7,6 +8,15 @@ def getHTML(url):
 
 html = getHTML("http://books.toscrape.com/")
 
-soup = BeautifulSoup(html,'html.parser')
+soup = BeautifulSoup(html,'lxml')
 
 table = soup.find()
+
+books = []
+
+with open("books.csv","w",newline="",encoding="utf-8") as file:
+
+    writer = csv.writer(file)
+    writer.writerow(books[0].__dict__)
+    for x in range(0,len(books)):
+        writer.writerow(books[x].__dict__.values())
