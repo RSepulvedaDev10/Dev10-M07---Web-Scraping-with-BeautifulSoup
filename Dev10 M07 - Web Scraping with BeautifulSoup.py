bd7d2f0b0598209a3e9c21daf03a922a4b7ebac5
RSepulvedaDev10 <94391613+RSepulvedaDev10@users.noreply.github.com>
2022-01-06 20:02:05 -0600

    Update Dev10 M07 - Web Scraping with BeautifulSoup.py

--- Dev10 M07 - Web Scraping with BeautifulSoup.py
+++ Dev10 M07 - Web Scraping with BeautifulSoup.py
@@ -2,21 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-def getHTML(url):
-    response = requests.get(url)
-    return response.text
+pages = []
 
-html = getHTML("http://books.toscrape.com/")
+for i in range(1,51):
+    url = f"http://books.toscrape.com/catalogue/page-{i}index.html"
+    
+    response = requests.get(url).text
 
-soup = BeautifulSoup(html,'lxml')
+    soup = BeautifulSoup(response,'html.parser')
 
-table = soup.find()
+    books = soup.find('ol', attrs = {'class': 'row'})
 
-books = []
+    for row in books.find_all("li"):
+        
+        for line in row.find_all("div", attrs = {"class":"product_price"}):
+            priceFind = line.find_all("p")
+            priceCheck = priceFind[1].text
+            
+        if priceCheck.strip() != "In Stock":
+            continue
+        else:
+            book = {}
+            
+            book['title'] = row.find("h3").find("a").attrs["title"]
+            book['price'] = priceFind[0].string
+            book['rating'] = row.find('p').attrs['class'][1]
+            book.append(pages)
+        
+print(pages)
 
-with open("books.csv","w",newline="",encoding="utf-8") as file:
 
-    writer = csv.writer(file)
-    writer.writerow(books[0].__dict__)
-    for x in range(0,len(books)):
-        writer.writerow(books[x].__dict__.values())
+keys = books[0].keys()
+with open("books.csv","w",newline="",encoding="utf-8") as csvFile:
+    writer = csv.DictWriter(csvFile. keys)
+    writer.writeheader()
+    writer.writetows(pages)
+    
